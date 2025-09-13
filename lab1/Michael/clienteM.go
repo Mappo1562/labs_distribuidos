4583e90af4e79e9b77d7201aae7a5b770b03a436
Alejandro Sanchez <alesanchezyo@gmail.com>
2025-09-13 17:25:00 -0300

    Ruta reportes cambiada

--- lab1/Michael/clienteM.go
+++ lab1/Michael/clienteM.go
@@ -78,8 +78,8 @@ func confirmarPago(addr string, nombre string, botinTotal int64, pago int64) boo
 ////////////////////////////////////////////////////////////////////////////
 
 func generarReporte(exito bool, botinBase int64, botinExtra int64, faseFallo string, motivoFallo string, confLes bool, confFran bool, confTre bool) bool {
-	file, err := os.Create("Reporte.txt")
 
+	file, err := os.Create("/app/reportes/Reporte.txt")
 	if err != nil {
 		panic(err)
 	}
