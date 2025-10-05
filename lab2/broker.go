7d193931c033cf8fc86a10bcd9722b0c792e142d
Alejandro <alejandro.vergara.araneda@gmail.com>
2025-10-05 11:40:55 -0300

    :shark:

---
+++ lab2/broker.go
@@ -0,0 +1,136 @@
+// ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣠⣄⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+// ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣠⣾⣿⣿⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+// ⠀⠀⠀⠀⠀⠀⠀⠀⢀⣀⣀⣀⣀⣀⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣰⣿⣿⣿⣿⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+// ⢠⣾⣿⣏⠉⠉⠉⠉⠉⠉⢡⣶⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠘⠻⢿⣿⣿⣿⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣤⡄⠀
+// ⠈⣿⣿⣿⣿⣦⣽⣦⡀⠀⠀⠛⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠉⠛⢧⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣠⣿⣿⠀⠀
+// ⠀⠘⢿⣿⣿⣿⣿⣿⣿⣦⣄⣀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣾⣿⣿⠇⠀⠀
+// ⠀⠀⠈⠻⣿⣿⣿⣿⡟⢿⠻⠛⠙⠉⠋⠛⠳⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣠⣿⣿⣿⡟⠀⠀⠀
+// ⠀⠀⠀⠀⠈⠙⢿⡇⣠⣤⣶⣶⣾⡉⠉⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⣰⣰⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠠⠾⢇⠀⠀⠀⠀⠀⣴⣿⣿⣿⣿⠃⠀⠀⠀
+// ⠀⠀⠀⠀⠀⠀⠀⠱⣿⣿⣿⣿⣿⣿⣦⡀⠀⠀⠀⠀⠀⠀⠀⠀⣰⣿⣿⡇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠐⠤⢤⣀⣀⣀⣀⣀⣀⣠⣤⣤⣤⣬⣭⣿⣿⠀⠀⠀⠀
+// ⠀⠀⠀⠀⠀⠀⠀⠀⠈⠛⢿⣿⣿⣿⣿⣿⣶⣤⣄⣀⣀⣠⣴⣾⣿⣿⣿⣷⣤⣀⡀⠀⠀⠀⠀⠀⠀⣀⣀⣤⣾⣿⣿⣿⣿⡿⠿⠛⠛⠻⣿⣿⣿⣿⣇⠀⠀⠀
+// ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⠙⠻⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣶⣶⣤⣤⣘⡛⠿⢿⡿⠟⠛⠉⠁⠀⠀⠀⠀⠀⠈⠻⣿⣿⣿⣦⠀⠀
+// ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣴⣾⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠿⢿⣿⣿⣿⣿⣿⣶⣦⣤⣀⡀⠀⠀⠀⠀⠀⠀⠀⠀⠈⠻⣿⣿⡄⠀
+// ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢠⣾⣿⣿⣿⠿⠛⠉⠁⠀⠈⠉⠙⠛⠛⠻⠿⠿⠿⠿⠟⠛⠃⠀⠀⠀⠉⠉⠉⠛⠛⠛⠿⠿⠿⣶⣦⣄⡀⠀⠀⠀⠀⠀⠈⠙⠛⠂
+// ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠠⠿⠛⠋⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠉⠉⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+
+package main
+
+// protoc --go_out=. --go-grpc_out=. .\prueba.proto
+// go mod init <nombre_carpeta_cliente>
+// go mod tidy
+import (
+	"context"
+	pb "en/alguna/parte"
+	"fmt"
+	"log"
+	"math/rand"
+	"net"
+	"strings"
+	"sync"
+	"time"
+
+	"google.golang.org/grpc"
+)
+
+const (
+	port = ":50051"
+)
+
+type server struct {
+	pb.UnimplementedBroker
+}
+
+var (
+	mu          sync.Mutex
+	registrados map[string]string // queria agregarle contraseña pero pisco piscola wea, el segundo valor es el rol nomas
+)
+
+func (s *server) Registrarse(ctx context.Context, in *pb.Registro) (*pb.Bool, error) {
+	_, ok := registrados[in.nombre]
+	if ok {
+		log.Printf("Entidad ya registrada, error.")
+		err := fmt.Errorf("No se pudo registrar la entidad en el broker, ya estaba registrada.\n")
+		return &pb.Bool{flag: false}, err
+	}
+	registrados[in.Nombre] = in.rol
+	return &pb.Bool{Value: true}, nil
+}
+
+// identidad y oferta son un ID
+func (s *server) GenerarOferta(ctx context.Context, in *pb.Oferta) (*pb.Bool, error) {
+	if contador%3 == 0 && contador != 0 {
+		log.Printf("voy a buscar, espera...")
+		time.Sleep(10 * time.Second)
+	}
+	if rand.Float64() > 0.9 {
+		msg := "No hay ofertas actualmente... \n"
+		log.Printf("%s", msg)
+		return &pb.OperationResponse{Oferta: &msg}, nil
+	}
+	contador += 1
+
+	var (
+		msg           string
+		exitoTrevor   *int64
+		exitoFranklin *int64
+		botin         *int64
+	)
+	if scanner.Scan() {
+		fmt.Println("Propuesta obtenida:", scanner.Text())
+		arr := strings.Split(scanner.Text(), ",")
+		botin = parseIntOpt(arr[0])
+		exitoFranklin = parseIntOpt(arr[1])
+		exitoTrevor = parseIntOpt(arr[2])
+		riesgo = parseIntOpt(arr[3])
+	} else {
+		e := scanner.Err()
+		if e != nil {
+			fmt.Println("Error al leer el archivo:", e)
+			msg = "Error al buscar atracos"
+		} else {
+			fmt.Println("Fin del archivo")
+			msg = "No quedan atracos disponibles"
+		}
+		exitoTrevor = nil
+		exitoFranklin = nil
+		riesgo = nil
+		botin = nil
+	}
+	// extras :v
+	lugares := [5]string{"Liberty city", "Los santos", "San Andreas", "Vice City", "Cayo Perico"}
+	objetivo := [4]string{"un banco", "una joyeria", "una mansión", "un barco"}
+
+	log.Printf("Encontré una opción, es en %v y se trata de %v, el botín esperado sería %v, pero hay un riesgo asociado de %v, si va trevor las probabilidades de exito son %v y si va franklin son %v \n", lugares[rand.Intn(5)], objetivo[rand.Intn(4)], formatInt64(botin), formatInt64(riesgo), formatInt64(exitoTrevor), formatInt64(exitoFranklin))
+	msg = fmt.Sprintf("Encontré una opción, es en %v y se trata de %v, el botín esperado sería %v, pero hay un riesgo asociado de %v, si va trevor las probabilidades de exito son %v y si va franklin son %v \n", lugares[rand.Intn(5)], objetivo[rand.Intn(4)], formatInt64(botin), formatInt64(riesgo), formatInt64(exitoTrevor), formatInt64(exitoFranklin))
+
+	return &pb.OperationResponse{Oferta: &msg, ExitoTrevor: exitoTrevor, ExitoFranklin: exitoFranklin, Riesgo: riesgo, Botin: botin}, nil
+}
+
+func (s *server) ConfirmarPago(ctx context.Context, in *pb.Pago) (*pb.ConfirmacionPago, error) {
+	var botinTotal int64 = in.BotinTotal
+	var pagoRecibido int64 = in.Pago
+	var pagoReal int64 = (botinTotal / int64(4)) + (botinTotal % int64(4))
+	if pagoRecibido == pagoReal {
+		return &pb.ConfirmacionPago{Confirma: true}, nil
+	} else {
+		return &pb.ConfirmacionPago{Confirma: false}, nil
+	}
+}
+
+func main() {
+	productores_registrados = make(map[string]string)
+
+	lis, err := net.Listen("tcp", port)
+	if err != nil {
+		log.Fatalf("conexión fallida:\n %v", err)
+	}
+
+	grpcServer := grpc.NewServer()
+	pb.RegisterPruebaServer(grpcServer, &server{})
+	pb.RegisterEstrellasServer(grpcServer, &server{})
+	pb.RegisterPagoBotinServer(grpcServer, &server{})
+	fmt.Println("server en ", port)
+	if err := grpcServer.Serve(lis); err != nil {
+		log.Fatalf("conexión fallida:\n %v", err)
+	}
+}
