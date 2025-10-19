01c378457d7a3d26d5165a01d4788435bdab6b59
Alejandro Sanchez <alesanchezyo@gmail.com>
2025-10-18 21:23:12 -0300

    Consumidor leer csv

--- lab2/consumidores/consumidor.go
+++ lab2/consumidores/consumidor.go
@@ -3,19 +3,110 @@ package main
 import (
 	pb "consumidores/proto"
 	"context"
+	"encoding/csv"
 	"fmt"
 	"log"
+	"os"
+	"strconv"
+	"strings"
 	"time"
 
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/credentials/insecure"
 )
 
+type Consumidor struct {
+	id_consumidor   string
+	categoria       []string
+	tiendas         []string
+	precio_max      int64
+	archivo_ofertas string
+}
+
+type server struct {
+	//pb.UnimplementedConsumidorServer
+	Grupo        int
+	Consumidores []Consumidor
+}
+
+var consumidores []Consumidor
+
+func leerConsumidores(path string) ([]Consumidor, error) {
+
+	archivo, err := os.Open(path)
+
+	if err != nil {
+		return nil, err
+	}
+
+	defer archivo.Close()
+
+	lector := csv.NewReader(archivo)
+	lector.TrimLeadingSpace = true
+
+	filas, err := lector.ReadAll()
+
+	if err != nil {
+		return nil, err
+	}
+
+	var ayuda []Consumidor
+
+	for i, fila := range filas {
+		if i == 0 {
+			continue
+		}
+
+		id := strings.TrimSpace(fila[0])
+		categorias := parseCampoMultiple(fila[1])
+		tiendas := parseCampoMultiple(fila[2])
+		precio_max := parsePrecio(fila[3])
+
+		c := Consumidor{
+			id_consumidor:   id,
+			categoria:       categorias,
+			tiendas:         tiendas,
+			precio_max:      precio_max,
+			archivo_ofertas: fmt.Sprintf("consumidor_%s_ofertas.csv", id),
+		}
+
+		ayuda = append(ayuda, c)
+
+	}
+
+	return ayuda, nil
+}
+
+func parseCampoMultiple(campo string) []string {
+	campo = strings.TrimSpace(campo)
+
+	if campo == "" || campo == "null" {
+		return nil
+	}
+
+	partes := strings.Split(campo, ";")
+	for i := range partes {
+		partes[i] = strings.TrimSpace(partes[i])
+	}
+
+	return partes
+}
+
+func parsePrecio(campo string) int64 {
+	campo = strings.TrimSpace(campo)
+
+	if campo == "" || campo == "null" {
+		return -1
+	}
+
+	val, _ := strconv.ParseInt(campo, 10, 64)
+	return val
+}
 func main() {
 
 	conn, err := grpc.NewClient("broker:50050", grpc.WithTransportCredentials(insecure.NewCredentials()))
 	if err != nil {
-		log.Fatalf("No se pueeo conectar: %v", err)
+		log.Fatalf("No se pudo conectar: %v", err)
 	}
 
 	defer conn.Close()
@@ -39,5 +130,17 @@ func main() {
 	} else {
 		fmt.Println("Falló el registro")
 		return
+	}
+
+	consumidores, err = leerConsumidores("consumidores.csv")
+
+	if err != nil {
+		log.Fatalf("Error leyendo los consumidores: %v", err)
+	}
+
+	for i := range len(consumidores) {
+		c := consumidores[i]
+
+		fmt.Printf("El consumidor %s se recupero correctamente\n", c.id_consumidor)
 	}
 }
