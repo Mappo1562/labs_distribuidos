9158e1ff4de5b55df99942e0bc7810aa62077d0d
Alejandro <alejandro.vergara.araneda@gmail.com>
2025-11-11 22:39:07 -0300

    nodos hacen consenso para definir un lider :shark:

---
+++ lab3/dbATC/dbATC.go
@@ -0,0 +1,158 @@
+// ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣠⣄⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+// ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣠⣾⣿⣿⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+// ⠀⠀⠀⠀⠀⠀⠀⠀⢀⣀⣀⣀⣀⣀⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣰⣿⣿⣿⣿⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+// ⢠⣾⣿⣏⠉⠉⠉⠉⠉⠉⢡⣶⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠘⠻⢿⣿⣿⣿⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣤⡄⠀
+// ⠈⣿⣿⣿⣿⣦⣽⣦⡀⠀⠀⠛⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠉⠛⢧⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣠⣿⣿⠀⠀
+// ⠀⠘⢿⣿⣿⣿⣿⣿⣿⣦⣄⣀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣾⣿⣿⠇⠀⠀
+// ⠀⠀⠈⠻⣿⣿⣿⣿⡟⢿⠻⠛⠙⠉⠋⠛⠳⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣠⣿⣿⣿⡟⠀⠀⠀
+// ⠀⠀⠀⠀⠈⠙⢿⡇⣠⣤⣶⣶⣾⡉⠉⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⣰⣰⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠠⠾⢇⠀⠀⠀⠀⠀⣴⣿⣿⣿⣿⠃⠀⠀⠀
+// ⠀⠀⠀⠀⠀⠀⠀⠱⣿⣿⣿⣿⣿⣿⣦⡀⠀⠀⠀⠀⠀⠀⠀⠀⣰⣿⣿⡇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠐⠤⢤⣀⣀⣀⣀⣀⣀⣠⣤⣤⣤⣬⣭⣿⣿⠀⠀⠀⠀
+// ⠀⠀⠀⠀⠀⠀⠀⠀⠈⠛⢿⣿⣿⣿⣿⣿⣶⣤⣄⣀⣀⣠⣴⣾⣿⣿⣿⣷⣤⣀⡀⠀⠀⠀⠀⠀⠀⣀⣀⣤⣾⣿⣿⣿⣿⡿⠿⠛⠛⠻⣿⣿⣿⣿⣇⠀⠀⠀
+// ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⠙⠻⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣶⣶⣤⣤⣘⡛⠿⢿⡿⠟⠛⠉⠁⠀⠀⠀⠀⠀⠈⠻⣿⣿⣿⣦⠀⠀
+// ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣴⣾⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠿⢿⣿⣿⣿⣿⣿⣶⣦⣤⣀⡀⠀⠀⠀⠀⠀⠀⠀⠀⠈⠻⣿⣿⡄⠀
+// ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢠⣾⣿⣿⣿⠿⠛⠉⠁⠀⠈⠉⠙⠛⠛⠻⠿⠿⠿⠿⠟⠛⠃⠀⠀⠀⠉⠉⠉⠛⠛⠛⠿⠿⠿⣶⣦⣄⡀⠀⠀⠀⠀⠀⠈⠙⠛⠂
+// ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠠⠿⠛⠋⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠉⠉⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+
+package main
+
+import (
+	"context"
+	"log"
+	"math/rand"
+	"net"
+	"sync"
+	"time"
+
+	pb "dbATC/proto"
+
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
+)
+
+// ID = 0  será el lider, ese coordina todo
+const (
+	ID         = 0 // la idea es que sea 0 para el nodo 1, 1 para el 2 y asi
+	PortNodo1  = "db1ATC:50051"
+	PortNodo2  = "db2ATC:50052"
+	PortNodo3  = "db3ATC:50053"
+	PortBroker = "broker:50050"
+)
+
+var (
+	AddBD     = [3]string{PortNodo1, PortNodo2, PortNodo3}
+	BD        = []string{}
+	port      string
+	mulider   sync.Mutex
+	lider     = -1
+	clientes  [2]pb.NodoBDConsensoClient
+	contextos [2]context.Context
+)
+
+type server struct {
+	pb.UnimplementedNodoBDConsensoServer
+}
+
+func init() {
+	AddBD[ID] = AddBD[ID][6:]
+	log.Printf("AddBD[ID] = %s\n", AddBD[ID])
+	port = AddBD[ID]
+}
+
+func verLider() {
+	mulider.Lock() // quiza estos candados no sean necesarios
+	liderCopy := lider
+	mulider.Unlock()
+	if liderCopy == -1 {
+		log.Println("Postularé a lider")
+		aceptados := 0
+		var ids [2]int64
+		for i, cliente := range clientes {
+			resp, err := cliente.PostularALider(contextos[i], &pb.Postulante{Id: ID})
+			if err != nil {
+				log.Fatalf("Error al postular como lider: %v\n", err)
+			}
+			if resp.Id == ID {
+				aceptados = aceptados + 1
+			} else {
+				log.Println("Uno de los nodos no me acepto como lider")
+			}
+			ids[i] = resp.Id
+		}
+		if aceptados > 1 {
+			notificados := 0
+			log.Println("Fui aceptado como lider por los otros nodos, informandoles del cambio ...")
+			for i, cliente := range clientes {
+				resp, err := cliente.InformarNewLider(contextos[i], &pb.Postulante{Id: ID})
+				if err != nil {
+					log.Fatalf("Error al informar que soy el nuevo lider: %v\n", err)
+				}
+				if resp.Id == ID {
+					notificados = notificados + 1
+				} else {
+					log.Println("Uno de los nodos no acepto la notificacion de actualización de lider ¿?")
+				}
+			}
+			mulider.Lock()
+			lider = ID
+			mulider.Unlock()
+			log.Println("Activo como lider")
+			// go timeoutLider()  -> estaba pensando en cambiar al lider cada 10 segundos pero mejor un timeout donde si no responde una consulta de otra BD en x tiempo se busca otro lider
+		} else {
+			if ids[0] == ids[1] {
+				///////////////////////// HAY OTRO LIDER ACTIVO Y ESTE ESTABA MUERTO *<[:V
+			}
+		}
+	}
+}
+
+func ciclo() {
+	rand.Seed(ID)
+	for true {
+		time.Sleep(time.Duration(rand.Intn(5)+1) * time.Second)
+		verLider()
+
+	}
+}
+
+func main() {
+
+	lis, err := net.Listen("tcp", port)
+	if err != nil {
+		log.Fatalf("conexión fallida:\n %v", err)
+	}
+
+	grpcServer := grpc.NewServer()
+	pb.RegisterNodoBDConsensoServer(grpcServer, &server{})
+	log.Println("Nodo con consenso activo en el puerto ", port)
+
+	go func() { //////////////////////////////////////////////////////// Criterio de termino
+		time.Sleep(60 * time.Second)
+		log.Println("Tiempo cumplido. Deteniendo servidor...")
+		grpcServer.GracefulStop()
+	}()
+
+	for i, dir := range BD {
+		if i == ID {
+			continue
+		}
+		conn, err := grpc.NewClient(dir, grpc.WithTransportCredentials(insecure.NewCredentials()))
+		if err != nil {
+			log.Fatalf("No se pudo conectar a %v: %v", dir, err)
+		}
+
+		defer conn.Close()
+
+		clientes[i] = pb.NewNodoBDConsensoClient(conn)
+
+		ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+		contextos[i] = ctx
+		defer cancel()
+	}
+
+	go ciclo()
+
+	if err := grpcServer.Serve(lis); err != nil {
+		log.Fatalf("conexión fallida:\n %v", err)
+	}
+
+}
